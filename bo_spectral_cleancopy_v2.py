ecf56c824daa4d423505a3b97d7bb5b7b80cc010
Arpan Biswas <arpanbiswas52@gmail.com>
2022-08-29 13:24:46 -0400

    Update bo_spectral_cleancopy_v2.py

--- bo_spectral_cleancopy_v2.py
+++ bo_spectral_cleancopy_v2.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1mCPhG0nAYTYW8cLPWuctwHdz39vbsufM
 """
 
-#pip install torch torchvision
+pipenv install torch torchvision
 #pip install botorch #version 0.5.1
 #pip install gpytorch #version 1.6.0
 #pip install smt
